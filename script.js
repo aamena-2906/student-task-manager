4e1d6502dff1a6235d90df3e7c78b676626ab8b7
aamena-2906 <patelaamena3@gmail.com>
2026-01-12 02:47:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let taskList = document.getElementById("taskList");
+
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskText = taskInput.value;
+
+    if (taskText === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.textContent = taskText;
+
+    li.onclick = function () {
+        li.classList.toggle("completed");
+    };
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    deleteBtn.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+}
